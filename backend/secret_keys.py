b2e32f59604214fff557c81c934f2f7c2a59bcd0
Raghava Srivastava <raghava@lpu.in>
2025-11-13 16:40:08 +0530

    Initial commit - StreamSphere project

---
+++ backend/secret_keys.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class SecretKeys(BaseSettings):
+    COGNITO_CLIENT_ID: str = ""
+    COGNITO_CLIENT_SECRET: str = ""
+    REGION_NAME: str = ""
+    POSTGRES_DB_URL: str = ""
+    AWS_RAW_VIDEOS_BUCKET: str = ""
+    AWS_VIDEO_THUMBNAIL_BUCKET: str = ""
